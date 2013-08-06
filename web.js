91632d9e811a15af009a0a22495f4540044e518a
Puru Lingaiah <puru.lingaiah@gmail.com>
2013-08-06 01:58:36 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var filebuf =fs.readFileSync('./index.html');
+    response.send(filebuf.toString());
 });
 
 var port = process.env.PORT || 5000;
